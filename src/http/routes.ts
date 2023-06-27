3d3197b6500df083fd48e22b8c5d28cd3e41c2de
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2023-06-27 11:48:52 -0400

    jwt auth with middleware

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,12 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
-import { Profile } from './controllers/profile'
+import { profile } from './controllers/profile'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
   // Authenticated
-  app.get('/me', Profile)
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
