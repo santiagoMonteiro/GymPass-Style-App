c190216be9632bb9afc624c2fdd30fcc2ed95c27
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2023-07-10 19:21:09 -0400

    create check-in e2e test

---
+++ src/http/controllers/check-ins/create.test.ts
@@ -0,0 +1,37 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import { prisma } from '@/lib/prisma'
+
+describe('Create Check-In (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'JavaScript Gym',
+        latitude: -3.094161,
+        longitude: -60.0143956,
+      },
+    })
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: -3.094161,
+        longitude: -60.0143956,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
